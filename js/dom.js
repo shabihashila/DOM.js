e4c4a2fef6c69b02f3e3841d86d2dd21e8757806
HP <HP@DESKTOP-8E0FCDH>
2025-05-22 00:30:39 +0600

    dom.js

--- js/dom.js
+++ js/dom.js
@@ -1,3 +1,29 @@
-const liCollection = document.getElementsByTagName("li");
-console.log(liCollection);
-// getelementtagby j eita amra beshi ekta use korbona
+console.log("first file");
+
+const wholeLi = document.getElementsByTagName("li");
+console.log(wholeLi);
+
+const AllTitles = document.getElementsByClassName("section-title");
+console.log(AllTitles);
+
+const secondCase = document.getElementById("second-section");
+console.log(secondCase);
+
+secondCase.style.backgroundColor = "violet";
+
+const thirdCase = document.getElementById("third-section");
+console.log(thirdCase);
+
+thirdCase.style.color = "red";
+thirdCase.style.backgroundColor = "yellow";
+
+const fourthCase = document.getElementById("4th-section");
+console.log(fourthCase);
+
+fourthCase.style.backgroundColor = "green";
+
+// document querySelectorAll
+// querySelector
+
+const li = document.createElement("li");
+li.innerText = "my name is shila";
